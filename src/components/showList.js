b8e2f7783a3dd870d29d2524bc1d7e36edcab8a9
Sangyun <tkfkd4447@naver.com>
2021-07-05 12:25:11 +0900

    edit function
    함수 컴포넌트의 return 문 밖에서 랜더링해주는 부분을 제거하고, return문에서 랜더링 하도록 구현, 랜더링시 비용이 많이 드는 sumIncome, sumExpediture function을
    ShowList function 밖에 선언해줌으로써, 필요할 때만 사용할 수 있도록 구현

--- src/components/showList.js
+++ src/components/showList.js
@@ -2,90 +2,78 @@ import React,{useState} from 'react';
 import {useSelector} from 'react-redux';
 import MakeList from './makeList';
 
+const sumIncome = (lists,month) => {
+
+    const newlists = lists.filter((list)=>list.month===month&&list.type==='INCOME');
+    const incomeList = [];
+    newlists.forEach((list)=>{
+        incomeList.push(Number(list.amount));
+    })
+    const income = incomeList.reduce((acc,cur)=>{
+        return acc+cur;
+    },0)
+
+    return income;
+
+}//월별 수입 합계를 구해주는 함수
+
+const sumExpediture = (lists,month) => {
+
+    const newlists = lists.filter((list)=>list.month===month&&list.type==='EXPEDITURE');
+    const expeditureList = [];
+    newlists.forEach((list)=>{
+        expeditureList.push(Number(list.amount));
+    })
+    const expediture = expeditureList.reduce((acc,cur)=>{
+        return acc+cur;
+    },0)
+
+    return expediture;
+
+}//월별 지출 합계를 구해주는 함수
+
 const ShowList = ({month}) => {
 
     const [blueBtn,setBlueBtn] = useState(false);
     const [redBtn,setRedBtn] = useState(false);
+    const [type,setType] = useState('incomeExpediture');
 
-    const lists = useSelector((state)=>state.list)
-    
+    const lists = useSelector((state)=>state.list);
+
     const clickBlueBtn = () => {
+
+        if(blueBtn===true){
+            
+            setBlueBtn(!blueBtn);
+            setType('incomeExpediture');
+            return;
+        }
+
         setBlueBtn(true);
         setRedBtn(false);
-        
-        if(blueBtn===true){
-            setBlueBtn(false);
-        }
+        setType('INCOME');
     }
 
     const clickRedBtn = () => {
+
+        if(redBtn===true){
+            setRedBtn(!redBtn);
+            setType('incomExpediture');
+            return;
+        }
+
         setRedBtn(true);
         setBlueBtn(false);
-        
-        if(redBtn===true){
-            setRedBtn(false);
-        }
+        setType('EXPEDITURE');
     }
-
-    const showRightList = () => {
-        if(blueBtn===true){
-            return (
-                <>
-                    <MakeList type={'INCOME'} lists={lists} month={month}/>
-                </>
-            );
-        }else if(redBtn===true){
-            return (
-                <>
-                    <MakeList type={'EXPEDITURE'} lists={lists} month={month}/>
-                </>
-            );
-        }else if(blueBtn===false && redBtn===false){
-            return (
-                <>
-                    <MakeList type={'incomeExpediture'} lists={lists} month={month}/>
-                </>
-            );
-        }
-    }//버튼에 따른 필요한 함수 실행
-
-    const sumIncome = () => {
-
-        const newlists = lists.filter((list)=>list.month===month&&list.type==='INCOME');
-        const incomeList = [];
-        newlists.forEach((list)=>{
-            incomeList.push(Number(list.amount));
-        })
-        const income = incomeList.reduce((acc,cur)=>{
-            return acc+cur;
-        },0)
-
-        return income;
-
-    }//월별 수입 합계를 구해주는 함수
-
-    const sumExpediture = () => {
-
-        const newlists = lists.filter((list)=>list.month===month&&list.type==='EXPEDITURE');
-        const expeditureList = [];
-        newlists.forEach((list)=>{
-            expeditureList.push(Number(list.amount));
-        })
-        const expediture = expeditureList.reduce((acc,cur)=>{
-            return acc+cur;
-        },0)
-
-        return expediture;
-
-    }//월별 지출 합계를 구해주는 함수
-
+    
     return(
         <> 
             <div>
-                <button onClick={clickBlueBtn}>+{sumIncome()}</button>
-                <button onClick={clickRedBtn}>{sumExpediture()}</button>
+                <button onClick={clickBlueBtn}>+{sumIncome(lists,month)}</button>
+                <button onClick={clickRedBtn}>{sumExpediture(lists,month)}</button>
             </div>
-            <div>{showRightList()}</div>
+            <div><MakeList type={type} lists={lists} month={month}/></div>
         </>
     )
 }
