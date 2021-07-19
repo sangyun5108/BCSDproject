d8bc1df1610174a60bc7546ae3bc64d5bf3c2191
Sangyun <tkfkd4447@naver.com>
2021-07-19 12:03:00 +0900

    configureStore
    combineReducer를 사용하지 않고, configureReducer를 사용하였다.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,12 @@
-import { combineReducers } from 'redux';
 import {incomeExpeditureReducer} from './reducers/incomeExpeditureReducer';
 import  showListReducer  from './reducers/showListReducer';
 import { configureStore } from '@reduxjs/toolkit';
-const reducers = combineReducers(
-    {   incomeExpeditureReducer : incomeExpeditureReducer.reducer,
-        showListReducer : showListReducer,
-    }
-);
 
 const store = configureStore({
-    reducer : reducers
+    reducer : {
+        incomeExpeditureReducer : incomeExpeditureReducer.reducer,
+        showListReducer : showListReducer
+    }
 });
 
 
