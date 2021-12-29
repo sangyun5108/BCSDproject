702d92639b2ce3bd6879e55a446204c9f5658bf0
sangyun5108 <69062776+sangyun5108@users.noreply.github.com>
2021-12-29 11:59:07 +0900

    fix : addHistory창 띄울시 너비 변경 버그 수정

---
+++ src/Globalstyles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html,body {
+    width:100%;
+    height:100vh;
+  }
+`;
+
+export default GlobalStyle;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './Globalstyles';
 
 ReactDOM.render(
   <Provider store={store}>
       <BrowserRouter>
+        <GlobalStyle/>
         <App/>
       </BrowserRouter>
   </Provider>,
